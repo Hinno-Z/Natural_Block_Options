a4ad58547ca5588ff2c5a7393f5425df9f6d79a1
Hinno <17elizabeth.brown@gmail.com>
2023-10-24 13:45:59 -0500

    Initial commit

---
+++ Naturals/src/main/java/net/hinno/naturals/naturals.java
@@ -0,0 +1,55 @@
+package net.hinno.naturals;
+
+import com.mojang.logging.LogUtils;
+import net.hinno.naturals.block.modBlocks;
+import net.hinno.naturals.item.modCreativeTab;
+import net.hinno.naturals.item.modItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(naturals.MODID)
+public class naturals
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "naturals";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public naturals()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+
+        modCreativeTab.register (modEventBus);
+        modItems.register (modEventBus);
+        modBlocks.BLOCKS.register (modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {     }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
