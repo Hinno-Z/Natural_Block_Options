8a12696add3410d10ea7c45917b59d2e450cf81c
Hinno <147219686+Hinno-Z@users.noreply.github.com>
2023-10-28 09:56:40 -0500

    Cleaning up version ports

---
+++ Naturals/java/net/hinno/naturals/custom/tintSlab.java
@@ -0,0 +1,30 @@
+package net.hinno.naturals.custom;
+
+import net.hinno.naturals.block.modBlocks;
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.world.level.GrassColor;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+
+public class tintSlab extends SlabBlock {
+    public tintSlab(BlockBehaviour.Properties p_49795_) {
+        super(p_49795_);
+    }
+
+	@OnlyIn(Dist.CLIENT)
+	public static void blockColorLoad(RegisterColorHandlersEvent.Block event) {
+		event.getBlockColors().register((bs, world, pos, index) -> {
+			return world != null && pos != null ? BiomeColors.getAverageGrassColor(world, pos) : GrassColor.get(0.5D, 1.0D);
+		}, modBlocks.GRASS_SLAB.get());
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public static void itemColorLoad(RegisterColorHandlersEvent.Item event) {
+		event.getItemColors().register((stack, index) -> {
+			return GrassColor.get(0.5D, 1.0D);
+		}, modBlocks.GRASS_SLAB.get());
+	}
+}
